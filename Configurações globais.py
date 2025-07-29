a63be64d5c4f57d883f43ef50f58183e95885e33
Dyel caxtro <oliveiraadielson870@gmail.com>
2025-07-29 15:21:28 -0300

    Create Configurações globais.py

---
+++ Configurações globais.py
@@ -0,0 +1,4 @@
+BATCH_SIZE = 8 # Define o tamanho do 'batch' (lote) para o treinamento do modelo.
+EPOCHS = 5 # Define o número de épocas (passes completos pelo dataset de treinamento).
+LEARNING_RATE = 1e-4 # Define o número de épocas (passes completos pelo dataset de treinamento).
+IMG_SIZE = (128, 128) # Define o tamanho para o qual as imagens e máscaras serão redimensionadas.
